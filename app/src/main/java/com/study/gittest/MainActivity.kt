4b1493700aa338fb5efde983786daad0b608d2f7
lwb <18614005205@163.com>
2020-03-12 09:59:14 +0800

    git测试第一次提交

--- app/src/main/java/com/study/gittest/MainActivity.kt
+++ app/src/main/java/com/study/gittest/MainActivity.kt
@@ -2,11 +2,20 @@ package com.study.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val ss = object: Handler() {
+           override fun handleMessage(msg: Message) {
+               super.handleMessage(msg)
+           }
+        }
+
     }
 }
