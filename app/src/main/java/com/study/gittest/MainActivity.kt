1afe4738b5fc9990a1a1b3bdae89e9be31aa75e1
lwb <18614005205@163.com>
2020-03-12 22:40:55 +0800

    测试线程停止

--- app/src/main/java/com/study/gittest/MainActivity.kt
+++ app/src/main/java/com/study/gittest/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.study.gittest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = this::class.java.name
+    }
+
+    private var stopTag = true
+
+    private lateinit var thread: Thread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +26,52 @@ class MainActivity : AppCompatActivity() {
 
         val ss = object : Handler() {
             override fun handleMessage(msg: Message) {
-                super.handleMessage(msg)
             }
         }
 
+        thread = Thread(stopTagThread)
+        thread.start()
+
+    }
+
+
+    private val interruptedThread = object : Runnable {
+        override fun run() {
+            Thread.currentThread().isInterrupted
+        }
+
+    }
+
+    /**
+     * 标志法停止线程
+     */
+    private val stopTagThread = Runnable {
+        while (stopTag) {
+            Log.e(TAG, "stopTagThread  -- start")
+            Thread.sleep(5000)
+            Log.e(TAG, "stopTagThread  -- finish")
+        }
+        Log.e(TAG, "stopTagThread  -- stoped")
+    }
+
+    private val handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+
+        }
+    }
+
+    /**
+     * 线程停止的点击事件
+     */
+    fun stopClick(v: View) {
+        try {
+            thread.stop()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            Toast.makeText(this, "click", Toast.LENGTH_SHORT).show()
+            stopTag = false
+        }
     }
 
     /**
